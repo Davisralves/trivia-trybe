41ceb2ecbfdb7ac9b5bbccac0ff3bc3fb095d15e
Davisralves <davialves324@gmail.com>
2021-12-18 14:04:37 -0300

    colocando função do header em arquivo da api

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 import logo from '../../7FbY.gif';
-import { saveScore } from '../../services/Api';
+import { saveScore, getGravatar } from '../../services/Api';
 
 class Header extends Component {
-  constructor() {
-    super();
-    this.getGravatar = this.getGravatar.bind(this);
-  }
-
-  getGravatar(email) {
-    const gravatar = md5(email).toString();
-    return gravatar;
-  }
-
   render() {
     const { player, email, score, statePlayer } = this.props;
     saveScore(statePlayer);
@@ -27,7 +16,7 @@ class Header extends Component {
         <div className="gravatal-img">
           <img
             data-testid="header-profile-picture"
-            src={ `https://www.gravatar.com/avatar/${this.getGravatar(email)}` }
+            src={ `https://www.gravatar.com/avatar/${getGravatar(email)}` }
             alt="player"
           />
           <p
