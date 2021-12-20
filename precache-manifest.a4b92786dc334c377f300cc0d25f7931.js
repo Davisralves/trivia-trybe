bda225df4fcea0c690dc5752e33087629ea95aca
Davisralves <davialves324@gmail.com>
2021-12-20 17:14:18 -0300

    Updates

---
+++ precache-manifest.a4b92786dc334c377f300cc0d25f7931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b25445db4a23e8ebd7e844f6908b5b",
+    "url": "/trivia-trybe/index.html"
+  },
+  {
+    "revision": "90f92ce486131b9c7b92",
+    "url": "/trivia-trybe/static/css/main.99994644.chunk.css"
+  },
+  {
+    "revision": "0b826c35d3c8222124b8",
+    "url": "/trivia-trybe/static/js/2.3a1fe68c.chunk.js"
+  },
+  {
+    "revision": "5078ed3b90587f0ca34f00de2f5b1c42",
+    "url": "/trivia-trybe/static/js/2.3a1fe68c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90f92ce486131b9c7b92",
+    "url": "/trivia-trybe/static/js/main.8048e98b.chunk.js"
+  },
+  {
+    "revision": "24cc4e6de6c1cf7fe4b6",
+    "url": "/trivia-trybe/static/js/runtime-main.36d90bd9.js"
+  },
+  {
+    "revision": "f7d7b7e726ed728fca81ef507da1e457",
+    "url": "/trivia-trybe/static/media/7FbY.f7d7b7e7.gif"
+  }
+]);
